89f1492d38d02f728140532391db32da83827002
lovebabbar <barnakghosh83@gmail.com>
2024-01-17 21:33:57 +0530

    Create index.js
    JavaScript Loops

---
+++ Video 57/index.js
@@ -0,0 +1,50 @@
+console.log("Here is the program about Loops");
+
+// let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// Basic for Loop
+// for (let i = 0; i < 100; i++) {
+
+//     console.log(a + i);
+
+// }
+
+// for in loop
+// let obj ={
+//     name: "Barnak",
+//     role: "Developer",
+//     Company: "Tiger Analytics"
+// }
+
+// for (const key in obj) {
+//      {
+//         const element = obj[key];
+//         console.log(key, element);
+//     }
+// }
+
+// for of loop
+// for (const c of "Barnak") {
+//     console.log(c);
+
+// }
+
+
+// while loop
+// let i = 0;
+// while (i < 6) {
+//   console.log(i);
+//   i++;
+// }
+
+
+// do while loop
+// let i = 10;
+// do {
+//     console.log(i);
+//     i++;
+    
+// } while (i<6);
