770687d93b6d6aec6a7484730fc460545757f2bc
lovebabbar <barnakghosh83@gmail.com>
2024-01-17 17:39:30 +0530

    JavaScript Variables, Data Types & Objects

---
+++ Video 55/script.js
@@ -0,0 +1,52 @@
+console.log("Hello World, This is tutorial of JS - 2")
+
+let a = 5;
+// a = a+1; 
+let b = 6;
+let c = "Barnak";
+let _a = "Bruno";
+
+// This is not allowed
+// var 55a = "Rohan";
+// Js is case sensative
+
+// console.log(a+b+8)
+// console.log(typeof a, typeof b, typeof c)
+
+
+// let is block code
+{
+    let a=66;
+    console.log(a)
+}
+console.log(a)
+
+
+
+// Not allowed because a1 is constant
+// const a1 = 6;
+// a1 = a1 + 1;
+
+
+let x = "Harry Bhai";
+let y = 22
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    name : "Bruno",
+    "job code" : 4500,
+    "is_hansome" : true
+    
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
