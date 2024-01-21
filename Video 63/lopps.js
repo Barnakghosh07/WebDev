6188d770882628e94f3478d1740ebc24547e74b7
lovebabbar <barnakghosh83@gmail.com>
2024-01-21 17:09:26 +0530

    JavaScript Arrays

---
+++ Video 63/lopps.js
@@ -0,0 +1,31 @@
+let a = [1,23,45,22,34,29]
+
+// for (let index = 0; index <a.length; index++) {
+//     const element =a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach((value, index, a)=>{
+//     console.log(value, index, a)
+// })
+
+// let obj = {
+//     a : 1, 
+//     b : 2, 
+//     c : 3
+// }
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element)
+        
+//     }
+// }
+
+// foroflopp
+// for (const value of a) {
+//     console.log(value)
+// }
+
