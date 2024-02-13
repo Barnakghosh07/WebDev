8560b4581c83d1f5c068e67a4136c5b178800ab4
lovebabbar <barnakghosh83@gmail.com>
2024-02-13 21:58:31 +0530

    Video 72

---
+++ Video 72/script.js
@@ -0,0 +1,10 @@
+console.log("Script.js initializing")
+
+let boxes = document.getElementsByClassName("box") 
+
+console.log(boxes)
+
+// Array.from(boxes).forEach(e=>{
+//     console.log(e)
+
+// })
